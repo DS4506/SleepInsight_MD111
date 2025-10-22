756ddfe702009c9b86ccf5bdc67d71874af59c9f
DS4506 <davidsmith4506@gmail.com>
2025-10-22 12:54:41 -0500

    Initial Commit

---
+++ SleepInsights_Starter/SleepInsights_StarterApp.swift
@@ -0,0 +1,17 @@
+//
+//  SleepInsights_StarterApp.swift
+//  SleepInsights_Starter
+//
+//  Created by Willie Earl on 10/22/25.
+//
+
+import SwiftUI
+
+@main
+struct SleepInsights_StarterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
